abcbeda796a55a37d3ebb75d0d0eee5464500607
mlazore <martinlazore@gmail.com>
2022-04-24 18:38:00 -0400

    colors and responsive email submission added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import contact from "./Contact.css"
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
+
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const [done, setDone] =useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fakbro8', 'template_rko77pg', form.current, '0Q0y34D6sB5C1V8sE')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className="Title">
       <h1>Contact Me</h1>
     <div className="contact-form">
     <div className="c-right">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <input type="text" name="user_name" className='user' placeholder="Name"/>
         <input type="Email" name="user_email" className='user' placeholder='Email' />
         <textarea name="message" className='user' placeholder='Message'/>
         <input type="submit" value="Send" className='button'/>
+        <span>{done && "Thanks for contacting me!"}</span>
       </form>
     </div>
     </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,26 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Footer = () => {
   return (
     <>
+    
     <div class="container my-5">
       <footer class="text-center text-lg-start">
+        <h1>Martin Lazore</h1>
         <div class="container d-flex justify-content-center py-5">
-          <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
-            <i class="fab fa-facebook-f"></i>
-          </button>
-          <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
-            <i class="fab fa-youtube"></i>
-          </button>
-          <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
-            <i class="fab fa-instagram"></i>
-          </button>
-          <button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
-            <i class="fab fa-twitter"></i>
-          </button>
+          <a href='https://www.linkedin.com/in/martin-lazore/'target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+          <i class="fab fa-linkedin"></i>
+          </button></a>
+          <a href='https://github.com/mlazore?tab=repositories'target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-floating mx-2">
+          <i class="fab fa-github"></i>
+          </button></a>
         </div>
-        
-  
-          </footer>
-        </div>
+      </footer>
+    </div>
       </>
   )
 }
